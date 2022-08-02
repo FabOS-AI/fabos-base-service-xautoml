11d98d88d3e66d3cf244b4d9f8c2714bb69a1fc4
n14s <n14x@posteo.de>
2022-08-03 00:44:18 +0200

    add mirror action

--- xautoml/util/__init__.py
+++
@@ -1,5 +0,0 @@
-import re
-
-
-def internal_cid_name(cid: str) -> str:
-    return re.sub(r'0(\d)', r'\1', cid)
--- xautoml/util/flaml.py
+++
@@ -1,9 +0,0 @@
-class FLAMLUtils:
-
-    @staticmethod
-    def isBaseEstimator(pipe):
-        try:
-            from flaml.model import BaseEstimator
-            return isinstance(pipe, BaseEstimator)
-        except ImportError:
-            return False
